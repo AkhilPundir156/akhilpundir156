4156b38209d89fbeb36ec4fc57cc8e6b889ad79c
AkhilPundir156 <akhilpundir156@gmail.com>
2025-09-22 10:47:05 +0530

    [FIX] portfolio:  Fix Responsiveness

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = () => {
     const [navBarIcon, setNavBarIcon] = useState("home");
 
     return (
-        <div className="bg-first-bg selection:bg-primary-dot text-secondary-text h-[100vh] w-[100vw] overflow-y-scroll pb-[24px]">
+        <div className="bg-first-bg selection:bg-primary-dot text-secondary-text h-[100vh] w-[100vw] overflow-y-scroll pb-[24px]" style={{"scrollbarWidth": "none"}}>
             <Navbar navBarIcon={navBarIcon} setNavBarIcon={setNavBarIcon} />
             <div className="mx-auto mt-[100px] max-w-[560px] w-[95%]">
                 <Outlet />
